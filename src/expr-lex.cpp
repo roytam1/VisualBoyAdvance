252077b72cbbbf68dd365928fa0936afd44388fd
kxu <>
2006-09-03 21:52:25 +0000

    FIX: fixed compilation issues on Linux

--- src/expr-lex.cpp
+++ src/expr-lex.cpp
@@ -386,6 +386,7 @@ char *yytext;
 #ifndef __GNUC__
 #include <io.h>
 #define isatty _isatty
+#define fileno _fileno
 #endif
 
 char *exprString;
@@ -1285,7 +1286,7 @@ FILE *file;
 #if YY_NEVER_INTERACTIVE
         b->yy_is_interactive = 0;
 #else
-        b->yy_is_interactive = file ? (isatty( _fileno(file) ) > 0) : 0;
+        b->yy_is_interactive = file ? (isatty( fileno(file) ) > 0) : 0;
 #endif
 #endif
         }
